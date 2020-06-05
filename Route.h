df59417a194c7a5548deaa98390f27a40dbcd939
dmitryvak1 <62395123+dmitryvak1@users.noreply.github.com>
2020-06-05 22:10:47 +0400

    Update Route.h

--- Route.h
+++ Route.h
@@ -15,8 +15,8 @@ public:
 	vector<Way> Ways;
 	StoreHouse store;
 
-	void setStore(StoreHouse newStoreHouse) {
-		store = newStoreHouse;
+	void setStore(StoreHouse store) {
+		this->store=store;
 	}
 
 	void setMarket(Market newMarket) {
